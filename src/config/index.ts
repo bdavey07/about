a131a4377365ff2d00e622308292142444742764
Ben <Ben.davey0@gmail.com>
2025-02-24 20:47:09 -0500

    Ready to go

--- src/config/index.ts
+++ src/config/index.ts
@@ -32,43 +32,37 @@ export const SITE_CONTENT: SiteContent = {
   projects: [
     {
       name: "Homelab",
-      summary: "I buult a collection of servers and networking equipment that I use to learn and experiment with new technologies and to host services for my friends and family.",
+      summary: "I built and maintain a collection of servers and networking equipment that I use to learn and experiment with new technologies and to host services for my friends and family.",
       image: "/homelab.jpg",
       linkMore: "/homelab",
     },
     {
-      name: "Stock Car Racing Career",
-      summary: "Throughout high school and university, I built, raced and repaired many iterations of the #07 Bonestock Racecar that I drove at Deleware and Grand Bend Speedways",
+      name: "My Stock Car Racing Career",
+      summary: "Throughout high school and university, I built, raced and repaired many iterations of the #07 Bonestock Racecar that I drove at Deleware and Grand Bend Speedways.",
       image: "/racing.jpg",
       linkMore: "/racing",
     },
     {
       name: "Guitar Build and Repair",
-      summary: "A collection of guitars that I have built and repaired over the years.",
+      summary: "I have a collection of guitars that I enjoy working on including a couple that I have built and repaired over the years.",
       image: "/guitars.jpg",
       linkMore: "/guitar",
     },
     {
       name: "Electronics Projects",
-      summary: "As a hobby, I have tinkered with a variety of electroinics projects in my spare time to gain experience with electronics components and learn to repair electronic devices",
+      summary: "As a hobby, I love to tinker with a variety of electroinics projects to gain experience with electronic components and learn to repair electronic devices.",
       image: "/electronics.jpg",
       linkMore: "/electronics",
     },
     {
       name: "3D Printing Projects",
-      summary: "I have recently begun to experiment with 3D printing to sharpen my CAD skills and deepend my manufacturing understanding.",
+      summary: "I have recently begun to experiment with 3D printing to sharpen my CAD skills and deepen my physical design and manufacturing understanding.",
       image: "/print.jpg",
       linkMore: "/printing",
     },
     {
-      name: "Woodworking Projects",
-      summary: "A selection of woodworking projects I have undertaken.",
-      image: "/woodworking.jpg",
-      linkMore: "/woodworking",
-    },
-    {
       name: "This Website!",
-      summary: "When I got the request for a portfolio submission, I decided to use it as an opportunity to learn a new web development framework and webhosting.",
+      summary: "When I got the request for a portfolio submission, I decided to use it as an opportunity to learn a new web development framework and try out web hosting.",
       image: "/site.jpg",
       linkMore: "/website",
     },
@@ -81,7 +75,7 @@ export const SITE_CONTENT: SiteContent = {
       endDate: "Present",
       summary: [
         "Designing the various C4 subsystems that go into GDLS-C Light Armoured Vehicles from concept development and selection to design release.",
-        "Primary areas of focus in vehicle networking, communication systems and counter-UAS systems.",
+        "Primary areas of focus are vehicle networking, communication systems and Counter-UAS systems.",
         "Hands on experience working with test and integration teams to ensure the functionality and quality of the systems.",
       ],
     },
@@ -91,7 +85,7 @@ export const SITE_CONTENT: SiteContent = {
       startDate: "May 2022",
       endDate: "Jan 2023",
       summary: [
-        "Designed and intgrated C4ISR systems in the next generation of the Canadian Army's Headquarters environment.",
+        "Designed and integrated the C4ISR battle management and data management systems in the next generation of the Canadian Army's Headquarters environment.",
         "Worked directly with customer leadership and end users to define, build and test the solution to best meet Canadian Army's needs.",
       ],
     },
@@ -101,9 +95,10 @@ export const SITE_CONTENT: SiteContent = {
       startDate: "May 2019",
       endDate: "May 2022",
       summary: [
-        "Worked on the integration and test team for the Canadian Army's next generation of vehicle based C4ISR systems.",
-        "Integrated and tested C4ISR components in a Systems Integration Lab and in the field on Canadian Forces Bases.",
+        "Worked on the integration and test team for the Canadian Army's next generation of vehicle based C4ISR systems in a Systems Integration Lab and in the field on Canadian Forces Bases.",
         "Supported test events, field trials and demonstrations to Canadian Army leadership and end users.",
+        "Developed and maintained automated test scripts to ensure the quality of the systems.",
+        "Frequently worked to integrate new C4 technology that was being considered for use by the customer to evaluate its performance and compatibility with the existing systems.",
       ],
     },
     {
@@ -124,20 +119,23 @@ export const SITE_CONTENT: SiteContent = {
       In the projects section, you will find a selection of my personal projects and expereinces that I believe shows my hands on experience in mechanics and technology areas that I believe are relevant to the Innovation Cell team.
       I love to learn by trying new things, researching all that I can find about them and getting my hands dirty building and breaking things. I believe that this is what makes me the ideal candidate for the Innovation Cell team.
     `,
-    image: "/ben.jpg",
+    image: "/collage.jpg",
   },
   testimonials: [
     {
       description: `
-        "Ben is a great guy, he's always willing to help out and lend a hand. He's the first person I go to when I need help with a project."
+        "Ben is always tinkering with something." - Ben's Mom
       `,
     },
     {
       description: `
-        "Ben is a hard worker and a quick learner. He's always willing to take on new challenges and learn new things."
+        "Ben always has too many projects on the go at once." - Ben's Friend
+      `,
+    },
+    {
+      description: `
+        "Couldn't you have just done a Powerpoint or something for this?" - Ben's Wife
       `,
     }
   ],
 };
-
-// #5755ff
